5eba74af7ecaad64e8245fa50e54b6f07df0200e
Elyse Burmester <burmesterelyse@gmail.com>
2019-09-10 17:33:32 +0000

    Automatically backed up by Learn

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,27 +1,6 @@
-export default (state = [], action) => {
+import { combineReducers } from 'redux';
+import quotes from './quotes';
 
-  let index
-  let quote
-
-  switch (action.type) {
-    case "ADD_QUOTE":
-    return [...state, action.quote]
-
-    case "REMOVE_QUOTE":
-    return state.filter(quote => quote.id !== action.quoteId)
-
-    case "UPVOTE_QUOTE":
-    index = state.findIndex(quote => quote.id === action.quoteId)
-    quote = state[index]
-    return [...state.slice(0, index), Object.assign({}, quote, {votes: quote.votes += 1}), ...state.slice(index + 1)];
-
-    case "DOWNVOTE_QUOTE":
-    index = state.findIndex(quote => quote.id === action.quoteId)
-    quote = state[index]
-    if (quote.votes > 0){
-      return [...state.slice(0, index), Object.assign({}, quote, {votes: quote.votes -= 1}), ...state.slice(index + 1)]
-    }
-    default:
-    return state;
-  }
-}
+export default combineReducers({
+  quotes
+});
